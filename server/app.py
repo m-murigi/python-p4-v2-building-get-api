59a472ee082ab51b1ff8a5241606ab0eb7d447ae
Emmanuel <zacmac807@gmail.com>
2024-06-30 15:14:59 +0300

    complete task

--- server/app.py
+++ server/app.py
@@ -1,3 +1,114 @@
+# # server/app.py
+
+# from flask import Flask, jsonify, make_response
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# from models import db, User, Review, Game
+
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.json.compact = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+# @app.route('/')
+# def index():
+#     return "Index for Game/Review/User API"
+
+# # start building your API here
+
+
+# @app.route("/games")
+# def games():
+
+#     games = []
+#     for game in Game.query.all():
+#         game_dict = {
+#             "title": game.title,
+#             "genre": game.genre,
+#             "platform": game.platform,
+#             "price": game.price,
+#         }
+#         games.append(game_dict)
+
+#     response = make_response(games, 200, {"Content-Type": "application/json"})
+
+#     return response
+
+
+# # @app.route("/games/<int:id>")
+# # def game_by_id(id):
+# #     game = Game.query.filter(Game.id == id).first()
+
+# #     game_dict = {
+# #         "title": game.title,
+# #         "genre": game.genre,
+# #         "platform": game.platform,
+# #         "price": game.price,
+# #     }
+
+# #     response = make_response(game_dict, 200)
+
+# #     return response
+
+
+# @app.route("/games")
+# def games():
+
+#     games = []
+#     for game in Game.query.all():
+#         game_dict = game.to_dict()
+#         games.append(game_dict)
+
+#     response = make_response(games, 200, {"Content-Type": "application/json"})
+
+#     return response
+
+
+# # @app.route("/games/users/<int:id>")
+# # def game_users_by_id(id):
+# #     game = Game.query.filter(Game.id == id).first()
+# #     users = []
+# #     for review in game.reviews:
+# #         user = review.user
+# #         user_dict = user.to_dict(rules=("-reviews",))
+# #         users.append(user_dict)
+
+# #     response = make_response(users, 200)
+
+# #     return response
+
+
+# @app.route("/games/<int:id>")
+# def game_by_id(id):
+#     game = Game.query.filter(Game.id == id).first()
+
+#     game_dict = game.to_dict()
+
+#     response = make_response(game_dict, 200)
+
+#     return response
+
+
+# @app.route("/games/users/<int:id>")
+# def game_users_by_id(id):
+#     game = Game.query.filter(Game.id == id).first()
+
+#     # use association proxy to get users for a game
+#     users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+#     response = make_response(users, 200)
+
+#     return response
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
 # server/app.py
 
 from flask import Flask, jsonify, make_response
@@ -15,13 +126,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Index for Game/Review/User API"
 
+
 # start building your API here
+@app.route("/games")
+def games():
+
+    games = [game.to_dict() for game in Game.query.all()]
+
+    response = make_response(games, 200)
+
+    return response
 
 
-if __name__ == '__main__':
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(game_dict, 200)
+
+    return response
+
+
+@app.route("/games/users/<int:id>")
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    # use association proxy to get users for a game
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+    response = make_response(users, 200)
+
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
